a36a2cb41a3cd487cff5701c0f0d776a82f514e4
Mosimiloluwa Adebisi <Adebisimosimiloluwa@gmail.com>
2025-06-09 13:39:02 +0000

    I just pushed it to live the name changed and the favicon is still displ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="https://i.ibb.co/Q1xx0Qx/Iconic-Image-EDIT-1.png" type="image/png" />
+        <link rel="icon" href="https://i.ibb.co/Q1xx0Qx/Iconic-Image-EDIT-1.png?v=2" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
